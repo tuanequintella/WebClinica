fb03733048fbdfe29426c4547bfc99aa78ffc6d4
Tuane Quintella <q.tuane@gmail.com>
2013-08-21 14:18:28 -0300

    arrumando titulo nas views e trocando pra erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
   def datetime_string(date,time)
     "#{date.day}/#{date.month}/#{date.year} #{time.hour}:#{time.min}:#{time.sec}"
   end
+
+  def icon_title_for(title, icon)
+    content_for(:content_title) { content_tag(:i, "", class: icon) + " " + title }
+  end
 end
 

59a54f255fc53c7f35cad80141746a8748f184b9
Tuane Quintella <q.tuane@gmail.com>
2013-09-05 15:04:02 -0300

    fixes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,19 +6,25 @@ class Appointment < ActiveRecord::Base
 
   I18N_PATH = 'activerecord.attributes.appointment.'
   
-  after_save :update_records_last_appointment
+  after_save :increase_records_last_appointment
+  before_destroy :decrease_records_last_appointment
 
   def to_s
     self.scheduled_at
   end
 
-  def update_records_last_appointment
-    binding.pry
+  def increase_records_last_appointment
     dates = record.appointments.map(&:scheduled_at)
     if dates.max <= scheduled_at
       record.last_appointment = self
       record.save
     end
+  end
+
+  def decrease_records_last_appointment
+    remaining_apps = record.appointments.reject{ |e| e == self }.sort_by{ |ap| ap.scheduled_at }
+    record.last_appointment = remaining_apps.last
+    record.save
   end
 
 end

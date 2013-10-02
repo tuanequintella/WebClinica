759092f6a55b9b4bf0322eb18a72707d24546cf1
Tuane Quintella <q.tuane@gmail.com>
2013-10-02 07:28:38 -0300

    usando validate_timeleness

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'cancan'
 gem 'pry-rails'
 gem 'jquery-rails'
 gem 'enumerize'
+gem 'validates_timeliness'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/available_day.rb
+++ app/models/available_day.rb
@@ -2,12 +2,18 @@ class AvailableDay < ActiveRecord::Base
   extend Enumerize
 
   attr_accessible :day, :work_start_time, :work_end_time, :interval_start_time, :interval_end_time
-
   belongs_to :agenda
-  I18N_PATH = 'activerecord.attributes.available_day.'
   
   enumerize :day, in: {sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6}
 
+  validates_uniqueness_of :day, scope: :agenda_id, message: "Dia da semana já definido para essa agenda"
+  validates_presence_of :day, :work_start_time, :work_end_time
+  validates_time :work_start_time, :work_end_time
+  validates_time :interval_start_time, :after => :work_start_time
+  validates_time :interval_end_time, :after => :interval_start_time, :before => :work_end_time
+  validates_time :work_end_time, :after => :interval_end_time
+
+  I18N_PATH = 'activerecord.attributes.available_day.'
 
 end
 

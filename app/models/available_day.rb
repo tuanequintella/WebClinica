181645854eed01ba3124730c93fab47304d4e7cb
Tuane Quintella <q.tuane@gmail.com>
2013-10-02 07:28:05 -0300

    trocando day para enumerize

--- app/models/available_day.rb
+++ app/models/available_day.rb
@@ -1,13 +1,13 @@
 class AvailableDay < ActiveRecord::Base
+  extend Enumerize
 
   attr_accessible :day, :work_start_time, :work_end_time, :interval_start_time, :interval_end_time
 
   belongs_to :agenda
   I18N_PATH = 'activerecord.attributes.available_day.'
+  
+  enumerize :day, in: {sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6}
 
-  def self.days
-    [[I18n.t(I18N_PATH + 'sunday'), 0], [I18n.t(I18N_PATH + 'monday'), 1], [I18n.t(I18N_PATH + 'tuesday'), 2], [I18n.t(I18N_PATH + 'wednesday'), 3], [I18n.t(I18N_PATH + 'thursday'), 4], [I18n.t(I18N_PATH + 'friday'), 5], [I18n.t(I18N_PATH + 'saturday'), 6]]
-  end
 
 end
 

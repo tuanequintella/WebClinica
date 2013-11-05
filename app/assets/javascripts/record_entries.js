f8603f781d0c3ac419bf955176c6067e5b98a34e
Tuane Quintella <q.tuane@gmail.com>
2013-11-05 02:28:42 -0200

    visualização e edição de prontuários

--- app/assets/javascripts/record_entries.js
+++ app/assets/javascripts/record_entries.js
@@ -8,4 +8,14 @@ $(function() {
       $("div#entry-window").modal();
     }); 
   });
+
+  $("a.edit-history-link").on('click', function (e) {
+    entry_id = $(e.currentTarget).data("entry");
+    $.ajax({
+      url: "/record_entries/" + entry_id + "/edit",
+    }).success(function(entry_view) {
+      $("div#entry-body").html(entry_view);
+      $("div#entry-window").modal();
+    }); 
+  });
 });
